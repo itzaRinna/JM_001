9884565bf4cd83c1b4d82a0dcd78bcca1b117d59
itzaRinna <dutchismahdick@gmail.com>
2024-05-24 10:34:02 +0700

    new:

---
+++ README.MD.txt
@@ -0,0 +1,17 @@
+### Javascript Mastery
+
+### To edit the file, do as the following:
+
+.Clone the repository:
+```
+git clone https://github.com/itzaRinna/JM_001.git
+```
+.Open VSCode and navigate to the source folder with:
+```bash
+cd source
+```
+.Run the developement with npm install and run dev:
+```bash
+npm install
+npm run dev
+```
--- source/src/App.tsx
+++ source/src/App.tsx
@@ -5,7 +5,7 @@ import { Navbar, Hero, Stats, Business, Billing,
  CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () => (
-  <div className="bg-primay w-full overflow-hidden">
+  <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar/>
