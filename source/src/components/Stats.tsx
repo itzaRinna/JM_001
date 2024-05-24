d110af620ceef9de1e927f9165127a1382b977ab
itzaRinna <dutchismahdick@gmail.com>
2024-05-24 21:22:46 +0700

    new

--- source/src/components/Stats.tsx
+++ source/src/components/Stats.tsx
@@ -2,7 +2,7 @@ import { stats } from "../constants";
 import styles from "../style";
 
 const Stats = () => (
-  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`} id="features">
     {stats.map((stat) => (
       <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3`}>
         <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] 
