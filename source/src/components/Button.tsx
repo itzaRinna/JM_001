9c276910d252fa6b8571f714e6732ce6fb0b4019
itzaRinna <dutchismahdick@gmail.com>
2024-05-24 18:46:39 +0700

    card deal done

--- source/src/components/Button.tsx
+++ source/src/components/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({ styles }) => {
   return (
     <button type='button' className={`font-poppins font-medium text-[14px]
-     text-primary py-4 px-6 bg-blue-gradient outline-none ${styles}`}>
+     text-primary py-4 px-6 bg-blue-gradient outline-none ${styles} rounded-[10px]`}>
       Get started
     </button>
   )
