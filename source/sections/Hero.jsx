9bce72e62b689bc2f288ef6ceaf4a6876b7acd86
itzaRinna <dutchismahdick@gmail.com>
2024-05-30 15:59:39 +0700

    done navbar now its hero time

--- source/sections/Hero.jsx
+++ source/sections/Hero.jsx
@@ -1,8 +1,26 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div variants={staggerContainer} initial="hidden" whileInView="show"
+    viewport={{ once:false, amount: 0.25}} 
+    className={`${styles.innerWidth} mx-auto flex flex-col`}>
+      <div className="flex flex-col relative justify-center items-center z-10">
+        <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} text-white`}>
+          Metaverse
+        </motion.h1>
+        <motion.h1 variants={textVariant(1.2)} className={`flex flex-row justify-center items-center`}>
+          <h1 className={styles.heroHeading}>
+            
+          </h1>
+        </motion.h1>
+      </div>
+    </motion.div>
   </section>
 );
 
