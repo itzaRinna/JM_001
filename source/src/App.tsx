d25b49c2c3f49fc251c9e20800c3b7d606671d6f
itzaRinna <dutchismahdick@gmail.com>
2024-06-19 17:23:41 +0700

    init again

--- source/src/App.tsx
+++ source/src/App.tsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CustomerReviews, Footer, Hero,
+ PopularProducts, Services, SpecialOffer,
+ Subscribe, SuperQuality } from "./sections";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <main className="relative">
+    {/* <Nav /> */}
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:p-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
 
-  return (
-    <>
-    
-    </>
-  )
-}
-
-export default App
+export default App;
